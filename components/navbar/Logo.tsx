89fea89e77e7fa1a41be7868061d9bfb941463dd
Anand-S23 <anandsroopsingh23@gmail.com>
2023-08-20 19:38:56 -0400

    Navbar UI

--- components/navbar/Logo.tsx
+++ components/navbar/Logo.tsx
@@ -1,20 +1,14 @@
 'use client';
 
 import { useRouter } from "next/navigation";
-import Image from "next/image";
 
 const Logo = () => {
     const router = useRouter();
 
     return (
-        <div className="flex flex-row items-center space-x-4 cursor-pointer">
-            <Image
-                src="/images/logo.png"
-                className="md:block hidden"
-                width="32"
-                height="32"
-                alt=""
-            />
+        <div className="flex flex-row items-center space-x-2 cursor-pointer">
+
+            <button className="bg-rose-500 hover:bg-rose-500 w-10 h-10 md:block hidden"></button>
 
             <h1 className="lg:block hidden text-2xl font-semibold">Frame</h1>
         </div>
