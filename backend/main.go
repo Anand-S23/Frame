f349c496977eef0df07622ebcdc3c7a447185c69
Anand-S23 <anandsroopsingh23@gmail.com>
2023-10-04 16:08:18 -0400

    Dockerizing backend + adding mongodb + connecting to mongodb

--- backend/main.go
+++ backend/main.go
@@ -1,36 +1,51 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/gorilla/mux"
-	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type Config struct {
     Port string
+    MongoURI string
 }
 
 func LoadEnv() *Config {
-    if err := godotenv.Load(); err != nil {
-        log.Fatal("Could not load env file")
+    port := os.Getenv("Port")
+    if port == "" {
+        port = "8080"
     }
 
-    cfg := &Config {
-        Port: os.Getenv("PORT"),
+    mongoURI := os.Getenv("MONGODB_URI")
+    if mongoURI == "" {
+        log.Fatal("MONGODB_URI variable must be specified in env file")
     }
 
-    if cfg.Port == "" {
-        log.Fatal("PORT variable missing from env file")
+    return &Config {
+        Port: port,
+        MongoURI: mongoURI,
     }
-
-    return cfg
 }
 
 func main() {
     cfg := LoadEnv()
+
+    opts := options.Client().ApplyURI(cfg.MongoURI)
+    client, err := mongo.Connect(context.TODO(), opts)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    err = client.Ping(context.TODO(), nil)
+    if err != nil {
+        log.Fatal(err)
+    }
 
     router := mux.NewRouter()
 	router.HandleFunc("/health_check", HandleFunc(checkHealth))
