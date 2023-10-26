642c5e698211a9c49a763c0db27b98453be50652
Anand-S23 <anandsroopsingh23@gmail.com>
2023-10-25 20:02:08 -0400

    Create form for registration in frontend

---
+++ frontend/src/routes/(auth)/register/+page.server.ts
@@ -0,0 +1,39 @@
+import { z } from 'zod';
+import { fail } from "@sveltejs/kit"
+import { superValidate } from "sveltekit-superforms/server"
+
+const registerSchema = z.object({
+    username: z
+        .string({ required_error: 'Name is required' })
+        .min(5, { message: "Username must be at least 5 characters" })
+        .max(25, { message: "Username must be less than 25 characters" })
+        .trim(),
+    email: z
+        .string({ required_error: 'Email is required' })
+        .max(64, { message: 'Email must be less than 64 characters' })
+        .email({ message: 'Email must be a valid email address' }),
+    password: z
+        .string({ required_error: 'Password is required' })
+        .min(8, { message: 'Password must be at least 8 characters' })
+        .max(32, { message: 'Password must be less than 32 characters' })
+        .trim(),
+});
+
+export const load = async (event) => {
+	const form = await superValidate(event, registerSchema);
+	return { form };
+}
+
+export const actions = {
+	default: async (event) => {
+		const form = await superValidate(event, registerSchema);
+		console.log(form);
+
+		if (!form.valid) {
+			return fail(400, { form });
+		}
+
+		return { form };
+	}
+}
+
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,3 +1,1 @@
-<h1 class="text-3xl font-bold underline">
-    Frame
-</h1>
+<h1>Frame</h1>
