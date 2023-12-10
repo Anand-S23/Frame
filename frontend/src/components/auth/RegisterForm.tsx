a209c90e4a5017845e3d770aec928349f14c1b51
Anand-S23 <anandsroopsingh23@gmail.com>
2023-12-09 21:08:01 -0500

    Implement onSubmit function for register form

--- frontend/src/components/auth/RegisterForm.tsx
+++ frontend/src/components/auth/RegisterForm.tsx
@@ -5,20 +5,64 @@ import { Input } from "@/components/ui/input";
 import { TRegisterSchema, RegisterSchema } from "@/lib/types";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
+import { useRouter } from "next/navigation";
+import { cn } from "@/lib/utils";
+
+const registerEndpoint = "http://localhost:8080/register";
 
 const RegisterForm = () => {
+    const router = useRouter();
+
     const {
         register,
         handleSubmit,
         formState: { errors },
-        reset,
+        setError
     } = useForm<TRegisterSchema>({
         resolver: zodResolver(RegisterSchema),
     });
 
     const onSubmit = async (data: TRegisterSchema) => {
-        // TODO: submit to server
-        reset();
+         const response = await fetch(registerEndpoint, {
+            method: "POST",
+            body: JSON.stringify(data),
+            headers: { "Content-Type": "application/json" },
+        });
+
+        if (!response.ok) {
+            // TODO: Error handling
+            return;
+        }
+
+        const responseData = await response.json();
+        if (responseData.errors) {
+            const errors = responseData.errors;
+
+            if (errors.username) {
+                setError("username", {
+                    type: "server", 
+                    message: errors.username
+                })
+            }
+
+            if (errors.email) {
+                setError("email", {
+                    type: "server", 
+                    message: errors.email
+                })
+            }
+
+            if (errors.password) {
+                setError("password", {
+                    type: "server", 
+                    message: errors.password
+                })
+            }
+        }
+
+        // TODO: potentially go striaght to login
+        // Redirect to homepage
+        router.push("/");
     };
 
     return (
@@ -27,7 +71,7 @@ const RegisterForm = () => {
                 <Input
                     {...register("username")}
                     type="text" id="username" name="username" placeholder="Username"
-                    className="mt-2 {$errors.username ? 'border-red-500' : ''}"
+                    className={cn("mt-2", errors.username ? 'border-red-500' : '')}
                 />
                 { errors.username && (
                     <p className="text-sm text-red-500 mx-2">
@@ -38,7 +82,7 @@ const RegisterForm = () => {
                 <Input 
                     {...register("email")}
                     type="email" id="email" name="email" placeholder="Email"
-                    className="mt-2 {$errors.email ? 'border-red-500' : ''}"
+                    className={cn("mt-2", errors.email ? 'border-red-500' : '')}
                 />
                 { errors.email && (
                     <p className="text-sm text-red-500 mx-2">
@@ -49,7 +93,7 @@ const RegisterForm = () => {
                 <Input 
                     {...register("password")}
                     type="password" id="password" name="password" placeholder="Password"
-                    className="mt-2 {$errors.password ? 'border-red-500' : ''}"
+                    className={cn("mt-2", errors.password ? 'border-red-500' : '')}
                 />
                 { errors.password && (
                     <p className="text-sm text-red-500 mx-2">
@@ -60,7 +104,7 @@ const RegisterForm = () => {
                 <Input 
                     {...register("confirm")}
                     type="password" id="confirm" name="confirm" placeholder="Confirm Password"
-                    className="mt-2 {$errors.password ? 'border-red-500' : ''}"
+                    className={cn("mt-2", errors.confirm ? 'border-red-500' : '')}
                 />
                 { errors.confirm && (
                     <p className="text-sm text-red-500 mx-2">
