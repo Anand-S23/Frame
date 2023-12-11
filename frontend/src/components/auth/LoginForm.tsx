6ff34335b13435395aa8c2ae6e00829d5d97b463
Anand-S23 <anandsroopsingh23@gmail.com>
2023-12-10 21:18:06 -0500

    Implement zod input validation schema for login form and the form itself

--- frontend/src/components/auth/LoginForm.tsx
+++ frontend/src/components/auth/LoginForm.tsx
@@ -1,9 +1,25 @@
 'use client';
 
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
+// const loginEndpoint = "http://localhost:8080/login";
+
 const LoginForm = () => {
     return (
-        <form>
-        </form>
+        <div className="flex flex-col w-full max-w-md gap-1.5 p-5">
+            <form>
+                <Input 
+                    type="email" id="email" name="email" placeholder="Email"
+                />
+
+                <Input 
+                    type="password" id="password" name="password" placeholder="Password"
+                />
+
+                <Button type="submit" className="mt-2">Submit</Button>
+            </form>
+        </div>
     );
 }
 
