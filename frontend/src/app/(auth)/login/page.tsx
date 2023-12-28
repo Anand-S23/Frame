82237d54fe4c7ad6216037a451de20cf42bb42b4
Anand-S23 <anandsroopsingh23@gmail.com>
2023-12-28 12:15:06 -0500

    Made login and register client components in order to use auth HOCs

--- frontend/src/app/(auth)/login/page.tsx
+++ frontend/src/app/(auth)/login/page.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import LoginForm from "@/components/auth/LoginForm";
 import withoutAuth from "@/hocs/withoutAuth";
 
--- frontend/src/app/(auth)/register/page.tsx
+++ frontend/src/app/(auth)/register/page.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import RegisterForm from "@/components/auth/RegisterForm";
 import withoutAuth from "@/hocs/withoutAuth";
 
